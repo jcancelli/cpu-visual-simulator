95ea1d8ce443c4d6adc1c4f838f39c0590ce7832
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-02 17:25:32 +0200

    added stringUtil

---
+++ src/util/stringUtil.ts
@@ -0,0 +1,11 @@
+// takes a possibly negative number and turns it into a valid index from 0 to length-1
+// example: pos=-2 length=9 returns=6
+export function positionToIndex(pos: number, length: number): number {
+	if (Math.abs(pos) > length) {
+		throw new Error(`Position ${pos} out of range +/-${length}`)
+	}
+	if (pos === 0) {
+		throw new Error("Positions start from 1")
+	}
+	return pos < 0 ? this.value.length + pos : pos - 1
+}
