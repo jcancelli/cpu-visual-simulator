7619b6fba6f5eeffb4b483f53c1023fbebea22ab
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-07 22:17:28 +0200

    fixed animation for AccToAlu1 action

--- src/execution/actions/cpu/AccToAlu1.ts
+++ src/execution/actions/cpu/AccToAlu1.ts
@@ -7,7 +7,7 @@ import CpuAction from "./CpuAction"
 export default class AccToAlu1 extends CpuAction {
 	protected async action(cache: Cache): Promise<any> {
 		cpuStore.setALU1(cache["ACC"])
-		await get(cpu).flash("ACC")
+		await get(cpu).flash("ALU:1")
 		cache["ALU:1"] = cache["ACC"]
 	}
 }
