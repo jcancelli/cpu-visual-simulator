989e610316cef6498566c7b975c96ed039b80c1d
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-10 22:41:08 +0200

    converted NodeMarker to tailwind

--- src/wires/NodeMarker.svelte
+++ src/wires/NodeMarker.svelte
@@ -5,54 +5,18 @@
 	export let node: Node
 </script>
 
-<div class="node" style="top: {node.y}px; left: {node.x}px;">
-	<div class="dot" title={`${node.name} x: ${node.x} y: ${node.y}`} />
-	<div class="text">
+<div class="absolute z-10" style="top: {node.y}px; left: {node.x}px;">
+	<div
+		class="absolute top-0 left-0 w-2 h-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white border border-fuchsia-500"
+		title={`${node.name} x: ${node.x} y: ${node.y}`}
+	/>
+	<div class="relative left-[7px] top-[-10px] bg-white font-bold">
 		{#if $debugStore.showNodeNames}
-			<span class="name">{node.name}</span>
+			<span class="text-fuchsia-500">{node.name}</span>
 		{/if}
 		{#if $debugStore.showNodesCoordinates}
-			<span class="x">x: {node.x}</span>
-			<span class="y">y: {node.y}</span>
+			<span class="text-blue-700">x: {node.x}</span>
+			<span class="text-red-700">y: {node.y}</span>
 		{/if}
 	</div>
 </div>
-
-<style lang="scss">
-	.node {
-		position: absolute;
-		z-index: 100;
-	}
-
-	.text {
-		position: relative;
-		left: 7px;
-		top: -10px;
-		background-color: white;
-		font-weight: bold;
-	}
-
-	.name {
-		color: fuchsia;
-	}
-
-	.x {
-		color: blue;
-	}
-
-	.y {
-		color: red;
-	}
-
-	.dot {
-		background-color: white;
-		border: 1px solid fuchsia;
-		position: absolute;
-		top: 0;
-		bottom: 0;
-		width: 6px;
-		height: 6px;
-		transform: translateX(-50%) translateY(-50%);
-		border-radius: 50%;
-	}
-</style>
