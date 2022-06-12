eb9f8e70ebf7fb858ced1a538de7ef9d2693f7bb
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-13 00:22:51 +0200

    added Debug and Execution buttons components

---
+++ src/components/basic/buttons/Debug.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let disabled = false
+</script>
+
+<button
+	class="
+        text-white
+        border
+        border-white
+        px-2
+        rounded-md
+        hover:text-black
+        hover:bg-white
+        transition-colors
+        disabled:brightness-[.6]
+        disabled:cursor-default
+        active:brightness-90
+        {$$restProps.class}
+    "
+	on:click
+	on:focus
+	on:focusout
+	on:mouseenter
+	on:mouseleave
+	on:mouseover
+	{disabled}
+>
+	<slot />
+</button>
---
+++ src/components/basic/buttons/Execution.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let disabled = false
+</script>
+
+<button
+	class="
+        flex
+        items-center
+        justify-center
+        w-9
+        h-9
+        border
+        border-black
+        rounded-md
+        bg-white
+        disabled:brightness-[.6]
+        disabled:cursor-default
+        active:brightness-90
+        {$$restProps.class}
+    "
+	on:click
+	on:focus
+	on:focusout
+	on:mouseenter
+	on:mouseleave
+	on:mouseover
+	{disabled}
+>
+	<slot />
+</button>
