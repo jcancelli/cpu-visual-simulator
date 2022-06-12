33f00cff17ce609ad1d24d3805ab5a467dc32f85
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-12 19:54:38 +0200

    tightened line height on ComponentLabel

--- src/components/ComponentLabel.svelte
+++ src/components/ComponentLabel.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <div
-	class="absolute w-fit font-bold cursor-default {FONT_SIZES[fontSize]}"
+	class="absolute w-fit font-bold cursor-default {FONT_SIZES[fontSize]} leading-tight"
 	style="top: {top}; left: {left}; bottom: {bottom}; right: {right}; font-size: {FONT_SIZES[
 		fontSize
 	]}"
