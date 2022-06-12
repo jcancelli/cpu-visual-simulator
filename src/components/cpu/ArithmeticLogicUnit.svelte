9defd8529b87e9d7a8dc2773fae054a37a25024e
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-12 23:33:50 +0200

    converted ArithmeticLogicUnit to tailwind

--- src/components/cpu/ArithmeticLogicUnit.svelte
+++ src/components/cpu/ArithmeticLogicUnit.svelte
@@ -8,7 +8,7 @@
 		return flash(
 			operand1Div,
 			"background-color",
-			{ r: 211, g: 211, b: 211 },
+			{ r: 224, g: 224, b: 224 },
 			{ r: 0, g: 255, b: 0 }
 		)
 	}
@@ -17,7 +17,7 @@
 		return flash(
 			operand2Div,
 			"background-color",
-			{ r: 211, g: 211, b: 211 },
+			{ r: 224, g: 224, b: 224 },
 			{ r: 0, g: 255, b: 0 }
 		)
 	}
@@ -26,7 +26,7 @@
 		return flash(
 			operatorDiv,
 			"background-color",
-			{ r: 211, g: 211, b: 211 },
+			{ r: 224, g: 224, b: 224 },
 			{ r: 0, g: 255, b: 0 }
 		)
 	}
@@ -39,73 +39,81 @@
 
 	$: {
 		if ($displaySettingsStore.binary) {
-			operand1 = $cpuStore.alu1 !== null ? $cpuStore.alu1.toBinaryString() : ""
-			operand2 = $cpuStore.alu2 !== null ? $cpuStore.alu2.toBinaryString() : ""
+			operand1 = $cpuStore.alu1 !== null ? splitBinString($cpuStore.alu1.toBinaryString()) : ""
+			operand2 = $cpuStore.alu2 !== null ? splitBinString($cpuStore.alu2.toBinaryString()) : ""
 		} else {
 			operand1 = $cpuStore.alu1 !== null ? "" + $cpuStore.alu1.signed() : ""
 			operand2 = $cpuStore.alu2 !== null ? "" + $cpuStore.alu2.signed() : ""
 		}
 	}
+
+	function splitBinString(bin: string): string {
+		return [bin.slice(0, 8), bin.slice(8)].join(" ")
+	}
 </script>
 
-<div class="alu">
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 450 162">
+<div class="absolute top-[380px] left-[210px] w-[300px] h-fit">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="stroke-black stroke-[1.6px] w-full h-full drop-shadow-component fill-gray-100"
+		viewBox="-1 -1 450 162"
+	>
 		<path d="m 0 0 l 192 0 l 32 32 l 32 -32 l 192 0 l -96 160 l -256 0 z" />
 	</svg>
-	<ComponentLabel text="ALU" top="-20px" left="45%" />
-	<div class="operand first" bind:this={operand1Div}>{operand1}</div>
-	<div class="operator" bind:this={operatorDiv}>{$cpuStore.operation}</div>
-	<div class="operand second" bind:this={operand2Div}>{operand2}</div>
+	<ComponentLabel text="ALU" top="-25px" left="45%" />
+	<div
+		class="
+			absolute
+			flex
+			items-center
+			justify-center
+			top-[15%]
+			left-[15%]
+			w-[90px] 
+			{$displaySettingsStore.binary ? 'top-[15%] h-[50px]' : 'top-[20%] h-[30px]'}
+			text-center
+			leading-tight
+			rounded-lg
+		"
+		bind:this={operand1Div}
+	>
+		{operand1}
+	</div>
+	<div
+		class="
+			absolute
+			flex
+			items-center
+			justify-center
+			top-[50%]
+			left-[50%]
+			-translate-x-1/2
+			w-[30px]
+			h-[30px]
+			rounded-lg
+			font-bold
+			text-2xl
+		"
+		bind:this={operatorDiv}
+	>
+		{$cpuStore.operation}
+	</div>
+	<div
+		class="
+			absolute
+			flex
+			items-center
+			justify-center
+			top-[15%]
+			right-[15%]
+			w-[90px]
+			{$displaySettingsStore.binary ? 'top-[15%] h-[50px]' : 'top-[20%] h-[30px]'}
+			text-center
+			leading-tight
+			rounded-lg
+		"
+		bind:this={operand2Div}
+	>
+		{operand2}
+	</div>
 </div>
-
-<style lang="scss">
-	@import "../../style/variables.scss";
-	@import "../../style/mixins.scss";
-
-	.alu {
-		position: absolute;
-		left: $cpu-alu-x;
-		top: $cpu-alu-y;
-		width: $cpu-alu-width;
-		height: $cpu-alu-height;
-
-		svg {
-			fill: $cpu-comp-bgcolor;
-			stroke-width: 1.6px;
-			stroke-linecap: round;
-			stroke: black;
-			filter: drop-shadow($cpu-comp-shadow);
-			height: 100%;
-			width: 100%;
-		}
-	}
-
-	.operand {
-		@include center-content;
-		position: absolute;
-		top: 20%;
-		width: 100px;
-		height: 30px;
-		border-radius: 10px;
-	}
-
-	.first {
-		left: 13%;
-	}
-
-	.second {
-		right: 13%;
-	}
-
-	.operator {
-		@include center-content;
-		position: absolute;
-		top: 50%;
-		left: 45%;
-		height: 30px;
-		width: 30px;
-		border-radius: 10px;
-		font-weight: bold;
-		font-size: 23px;
-	}
-</style>
