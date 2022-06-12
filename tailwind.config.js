0929513601830fa4da639facdaffaaf06962a9d6
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-12 19:45:42 +0200

    added custom gray color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
       },
       height: {
         "app": "800px"
+      },
+      boxShadow: {
+        "component": "0 0 10px rgba(0, 0, 0, 0.2)",
+        "cpu": "0 0 20px rgba(0, 0, 0, 0.3)"
+      },
+      colors: {
+        "gray": {
+          "100": "#e0e0e0",
+          "200": "#d0d0d0",
+          "300": "#c0c0c0",
+          "400": "#b0b0b0",
+          "500": "#a0a0a0",
+          "600": "#808080"
+        }
       }
     },
   },
