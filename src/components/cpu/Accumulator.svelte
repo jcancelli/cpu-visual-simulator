da95cfbd1e15944d307e5a93391da7f2afa55d42
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-12 19:48:04 +0200

    converted Accumulator to tailwind

--- src/components/cpu/Accumulator.svelte
+++ src/components/cpu/Accumulator.svelte
@@ -8,7 +8,7 @@
 		return flashComponent(
 			accDiv,
 			"background-color",
-			{ r: 211, g: 211, b: 211 },
+			{ r: 224, g: 224, b: 224 },
 			{ r: 0, g: 255, b: 0 }
 		)
 	}
@@ -16,23 +16,26 @@
 	let accDiv: HTMLDivElement
 </script>
 
-<div class="acc" bind:this={accDiv}>
-	<ComponentLabel text="ACC" top="-20px" left="0" />
+<div
+	class="
+		absolute
+		top-[550px]
+		left-[275px]
+		w-[170px]
+		h-[30px]
+		border
+		border-[black]
+		rounded-md
+		bg-gray-100
+		shadow-component
+		flex
+		items-center
+		justify-center
+	"
+	bind:this={accDiv}
+>
+	<ComponentLabel text="ACC" top="-25px" left="0" />
 	{$displaySettingsStore.binary
 		? $cpuStore.accumulator.toBinaryString()
 		: $cpuStore.accumulator.signed()}
 </div>
-
-<style lang="scss">
-	@import "../../style/variables.scss";
-	@import "../../style/mixins.scss";
-
-	.acc {
-		@include cpu-component;
-		@include center-content;
-		left: $cpu-acc-x;
-		top: $cpu-acc-y;
-		width: $cpu-acc-width;
-		height: $cpu-acc-height;
-	}
-</style>
