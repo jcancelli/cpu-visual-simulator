730aeb02522d7a451a28f9dc86a32dde6d4caf19
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-15 22:24:52 +0200

    added Widget debug component

---
+++ src/components/debug/Widget.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let title = ""
+</script>
+
+<div class="flex flex-col items-center gap-1.5 max-w-[250px]">
+	<h2 class="text-lg mx-auto">{title}</h2>
+	<div class="w-full flex flex-col items-center gap-0.5">
+		<slot name="inputs" />
+	</div>
+	<div class="w-full flex flex-wrap justify-center gap-1">
+		<slot name="buttons" />
+	</div>
+</div>
