34c769d517dd3106ff9e84665ff6b0edf441c087
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-16 23:43:51 +0200

    started implementing i18n

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -1,6 +1,8 @@
 import { writable } from "svelte/store"
+import { getDefaultLanguage } from "../resources/text"
 
 export const displayAsBinary = writable(false)
 export const displayLabels = writable(true)
 export const playAnimations = writable(true)
 export const animationSpeed = writable(1)
+export const language = writable(getDefaultLanguage())
