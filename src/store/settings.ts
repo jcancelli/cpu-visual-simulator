28f93c582bb5b78544e8b040c5b54bebea6aa8c9
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-19 10:50:46 +0200

    changed animationSpeed default value

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -4,6 +4,6 @@ import { getDefaultLanguage } from "../resources/text"
 export const displayAsBinary = writable(false)
 export const displayLabels = writable(true)
 export const playAnimations = writable(true)
-export const animationSpeed = writable(1)
+export const animationSpeed = writable(1.6)
 export const language = writable(getDefaultLanguage())
 export const textToSpeech = writable(false)
