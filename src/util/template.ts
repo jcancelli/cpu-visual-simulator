c07c514fd41ba381599ca3e480809216e8ef1039
jcancelli <jonathan.cancelli.46@gmail.com>
2022-06-21 18:11:34 +0200

    template.interpolate now allows values of type any

--- src/util/template.ts
+++ src/util/template.ts
@@ -1,7 +1,7 @@
-export function interpolate(base: string, ...values: string[]): string {
+export function interpolate(base: string, ...values: any[]): string {
 	let result = base
 	values.forEach((value: string, index: number) => {
-		result = result.replace(`{${index}}`, value)
+		result = result.replace(`{${index}}`, value.toString())
 	})
 	return result
 }
