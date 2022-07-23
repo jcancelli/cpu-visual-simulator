660a083b8a4c2b3f25cf5e06609281ace1afd5e3
jcancelli <jonathan.cancelli.46@gmail.com>
2022-07-23 21:21:17 +0200

    renamed busses stores

--- src/store/busses.ts
+++ src/store/busses.ts
@@ -1,15 +1,28 @@
 import { writable } from "svelte/store"
 import BinaryValue from "../util/BinaryValue"
 
-export const data_main = writable<BinaryValue>(new BinaryValue(16, 0))
-export const data_ir_cu = writable<BinaryValue>(new BinaryValue(16, 0))
-export const data_alu_acc = writable<BinaryValue>(new BinaryValue(16, 0))
-export const data_alu_sw = writable<BinaryValue>(new BinaryValue(16, 0))
-export const data_mux_alu = writable<BinaryValue>(new BinaryValue(16, 0))
+export const main_data_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+export const ir_cu_data_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+export const alu_acc_data_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+export const alu_sw_data_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+export const mux_alu_data_bus = writable<BinaryValue>(new BinaryValue(16, 0))
 
-export const ctrl_cu_mux = writable<BinaryValue>(new BinaryValue(16, 0))
-export const ctrl_cu_ram = writable<BinaryValue>(new BinaryValue(16, 0))
-export const ctrl_cu_alu = writable<BinaryValue>(new BinaryValue(16, 0))
+export const cu_mux_ctrl_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+export const cu_ram_ctrl_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+export const cu_alu_ctrl_bus = writable<BinaryValue>(new BinaryValue(16, 0))
 
-export const address_main = writable<BinaryValue>(new BinaryValue(16, 0))
-export const address_inc_pc = writable<BinaryValue>(new BinaryValue(16, 0))
+export const main_address_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+export const inc_pc_address_bus = writable<BinaryValue>(new BinaryValue(16, 0))
+
+export default {
+	main_data_bus,
+	ir_cu_data_bus,
+	alu_acc_data_bus,
+	alu_sw_data_bus,
+	mux_alu_data_bus,
+	cu_mux_ctrl_bus,
+	cu_ram_ctrl_bus,
+	cu_alu_ctrl_bus,
+	main_address_bus,
+	inc_pc_address_bus
+}
