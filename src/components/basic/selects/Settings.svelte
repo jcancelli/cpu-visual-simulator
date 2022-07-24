9b831badada6e96352719b7c719ce9d865f01359
jcancelli <jonathan.cancelli.46@gmail.com>
2022-07-24 19:50:42 +0200

    added Settings select component

---
+++ src/components/basic/selects/Settings.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let value = ""
+	export let placeholder = ""
+	export let disabled = false
+	export let options: string[] = []
+</script>
+
+<select
+	class="
+        cursor-pointer
+        bg-transparent
+        border-2
+        border-black
+        text-black
+        rounded-md
+        px-1
+        disabled:brightness-[.6]
+        w-fit
+        {$$restProps.class}
+    "
+	on:input
+	on:change
+	on:focus
+	on:focusout
+	on:mouseenter
+	on:mouseleave
+	on:mouseover
+	bind:value
+	{disabled}
+>
+	{#if placeholder !== ""}
+		<option class="text-black bg-white" value="" selected disabled>{placeholder}</option>
+	{/if}
+	{#each options as option}
+		<option class="text-black bg-white" value={option}>{option}</option>
+	{/each}
+</select>
