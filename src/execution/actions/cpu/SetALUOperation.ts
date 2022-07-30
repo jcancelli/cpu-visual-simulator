a638f844e927b305ca07322ce59e39439ac7d76c
jcancelli <jonathan.cancelli.46@gmail.com>
2022-07-30 16:59:55 +0200

    re-added mistakenly deleted files

---
+++ src/execution/actions/cpu/SetALUOperation.ts
@@ -0,0 +1,12 @@
+import { get } from "svelte/store"
+import { cpu } from "../../../store/components"
+import cpuStore from "../../../store/cpu"
+import CpuAction from "./CpuAction"
+
+export default class SetALUOperation extends CpuAction {
+	protected async action(): Promise<any> {
+		const operation = get(cpuStore.instructionRegister).opcode.operator
+		cpuStore.aluOperation.set(operation)
+		await get(cpu).flash("ALU:OPR")
+	}
+}
