1d2eaa26eed289bd2aeb2e59a6c71a5bc45f626a
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-07 16:29:05 +0200

    added docs page

--- rollup.config.js
+++ rollup.config.js
@@ -30,7 +30,7 @@ function serve() {
 	};
 }
 
-export default {
+const mainAppConfig = {
 	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
@@ -85,4 +85,47 @@ export default {
 	watch: {
 		clearScreen: false
 	}
-};
+}
+
+const docsConfig = {
+	input: 'docs/main.ts',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'public/build/docs.bundle.js'
+	},
+	plugins: [
+		svelte({
+			preprocess: sveltePreprocess({
+				sourceMap: !production,
+				postcss: {
+					plugins: [ require("tailwindcss")(), require("autoprefixer")() ]
+				}
+			}),
+			compilerOptions: {
+				dev: !production
+			}
+		}),
+		css({ output: 'dovs.bundle.css' }),
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+		typescript({
+			sourceMap: !production,
+			inlineSources: !production
+		}),
+		!production && livereload('public'),
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+}
+
+export default [
+	mainAppConfig,
+	docsConfig
+];
