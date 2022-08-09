34f4d573be42593353e16b7bfbc7496327f09182
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-09 12:46:18 +0200

    program counter component is now editable

--- src/components/cpu/ProgramCounter.svelte
+++ src/components/cpu/ProgramCounter.svelte
@@ -3,12 +3,72 @@
 	import ComponentLabel from "../labels/Component.svelte"
 	import { flash as flashElement } from "../../util/animationUtil"
 	import { displayAsBinary } from "../../store/settings"
+	import BinaryValue from "../../util/BinaryValue"
+	import Logger from "../../util/logger"
+	import { isValidAddress } from "../../util/ramUtil"
+	import CheckedError from "../../errors/CheckedError"
+	import { messageFeed } from "../../store/components"
 
-	export async function flash() {
+	let element: HTMLDivElement
+	let isEditing = false
+	let inputValue: string
+
+	$: onProgramCounterChange($programCounter)
+	$: onDisplayAsBinaryChange($displayAsBinary)
+
+	syncInputValue()
+
+	export async function flash(): Promise<void> {
 		return flashElement(element, "background-color", { r: 224, g: 224, b: 224 }, { r: 0, g: 255, b: 0 })
 	}
 
-	let element: HTMLDivElement
+	function commitEdit(): void {
+		try {
+			Logger.info(`ProgramCounter input: "${inputValue}"`, "USER_INPUT")
+			let newValue: BinaryValue
+			if ($displayAsBinary) {
+				newValue = new BinaryValue(8, inputValue)
+			} else {
+				newValue = new BinaryValue(8, parseInt(inputValue))
+			}
+			if (!isValidAddress(newValue.unsigned())) {
+				throw new CheckedError("asd") // todo
+			}
+			programCounter.set(newValue)
+		} catch (error) {
+			$messageFeed?.error(error.message)
+			Logger.error(error, "USER_INPUT", error.isChecked)
+			syncInputValue()
+		} finally {
+			isEditing = false
+		}
+	}
+
+	function onProgramCounterChange(newValue: BinaryValue): void {
+		syncInputValue()
+	}
+
+	function onDisplayAsBinaryChange(newValue: boolean): void {
+		syncInputValue()
+	}
+
+	function syncInputValue() {
+		inputValue = $displayAsBinary ? $programCounter.toBinaryString() : $programCounter.unsigned().toString()
+	}
+
+	function formatInput() {
+		inputValue = $displayAsBinary
+			? inputValue.replace(/[^10]/g, "").slice(0, 8)
+			: inputValue.replace(/\D/g, "").slice(0, 3)
+	}
+
+	function focus(node: HTMLElement): void {
+		node.focus()
+	}
+
+	function highlightText(node: HTMLInputElement): void {
+		node.select()
+	}
 </script>
 
 <div
@@ -28,7 +88,28 @@
 	justify-center
 "
 	bind:this={element}
+	on:click={() => (isEditing = true)}
 >
 	<ComponentLabel text="PC" top="-25px" left="0" />
-	{$displayAsBinary ? $programCounter.toBinaryString() : $programCounter.unsigned()}
+	{#if isEditing}
+		<input
+			type="text"
+			class="
+				w-[100px]
+				h-[30px]
+				rounded-md
+				bg-black
+				text-gray-200
+				text-center
+				selection:bg-transparent
+			"
+			use:focus
+			use:highlightText
+			bind:value={inputValue}
+			on:input={formatInput}
+			on:focusout={commitEdit}
+		/>
+	{:else}
+		{$displayAsBinary ? $programCounter.toBinaryString() : $programCounter.unsigned()}
+	{/if}
 </div>
