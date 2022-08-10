842693d9add12c43cde5da357a0991233a42a860
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-10 21:06:20 +0200

    customized gray color of the manual page tailwind

--- manual/tailwind.config.js
+++ manual/tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./public/manual.html", "./manual/**/*.svelte"],
-	theme: {},
+	theme: {
+		extend: {
+			colors: {
+				"gray": {
+					"100": "#e0e0e0",
+					"200": "#d0d0d0",
+					"300": "#c0c0c0",
+					"400": "#b0b0b0",
+					"500": "#a0a0a0",
+					"600": "#808080",
+					"700": "#707070",
+					"800": "#606060"
+				}
+			}
+		},
+	},
 	plugins: []
 }
