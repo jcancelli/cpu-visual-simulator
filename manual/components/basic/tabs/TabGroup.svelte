717fef8b399bb1ed50bd7d5bd67798a7e1d9078e
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-13 18:12:20 +0200

    implemented custom tab view

---
+++ manual/components/basic/tabs/TabGroup.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { TabGroup } from "@rgossiaux/svelte-headlessui"
+</script>
+
+<TabGroup class="grid grid-cols-tabgroup gap-x-4 {$$props.class}" vertical>
+	<slot />
+</TabGroup>
