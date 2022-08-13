fd4ba433f7b35ecbb81ef3b7b9ca10b9357d11e5
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-13 18:15:34 +0200

    moved MainTabView to its own component

--- manual/App.svelte
+++ manual/App.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-	import OpcodesTable from "./components/OpcodesTable.svelte"
-	import text from "./stores/text"
+	import Header from "./components/Header.svelte"
+	import MainTabView from "./components/MainTabView.svelte"
 </script>
 
-<header class="p-4">
-	<h1 class="text-5xl font-bold">{$text.title}</h1>
-</header>
-<main class="flex flex-col items-center">
-	<OpcodesTable />
+<Header />
+<main>
+	<MainTabView />
 </main>
 
 <style lang="postcss" global>
---
+++ manual/components/MainTabView.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import TabGroup from "./basic/tabs/TabGroup.svelte"
+	import TabList from "./basic/tabs/TabList.svelte"
+	import Tab from "./basic/tabs/Tab.svelte"
+	import TabPanels from "./basic/tabs/TabPanels.svelte"
+	import TabPanel from "./basic/tabs/TabPanel.svelte"
+	import text from "../stores/text"
+	import OpcodesTable from "./OpcodesTable.svelte"
+</script>
+
+<TabGroup class="p-5">
+	<TabList class="mt-4">
+		<Tab class="border-t border-t-gray-500">{$text.sections.instruction_set.title}</Tab>
+		<Tab>Examples</Tab>
+		<Tab>Playground</Tab>
+		<Tab>Credits</Tab>
+	</TabList>
+	<TabPanels>
+		<TabPanel>
+			<OpcodesTable class="mx-auto" />
+		</TabPanel>
+		<TabPanel>
+			<h1>Hello world</h1>
+		</TabPanel>
+	</TabPanels>
+</TabGroup>
