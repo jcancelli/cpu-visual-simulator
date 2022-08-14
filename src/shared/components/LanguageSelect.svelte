e929212e5d6b796965f0a7d3609d2be98fb5a8e4
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-14 11:05:00 +0200

    styled a little more the LanguageSelect component

--- src/shared/components/LanguageSelect.svelte
+++ src/shared/components/LanguageSelect.svelte
@@ -17,10 +17,12 @@
 	<ListboxButton>
 		<svelte:component this={flags.get(value)} class="w-12 h-7" />
 	</ListboxButton>
-	<ListboxOptions class="absolute flex flex-col gap-1 overflow-y-auto overflow-x-hidden h-28">
+	<ListboxOptions
+		class="absolute flex flex-col gap-1 py-2 px-1 overflow-y-auto overflow-x-hidden max-h-28 z-10 bg-gray-100 rounded-md shadow-md"
+	>
 		{#each SUPPORTED_LANGS as language (language)}
 			<ListboxOption value={language}>
-				<svelte:component this={flags.get(language)} class="w-12 h-7 cursor-pointer" />
+				<svelte:component this={flags.get(language)} class="w-12 h-7 cursor-pointer shadow-md" />
 			</ListboxOption>
 		{/each}
 	</ListboxOptions>
