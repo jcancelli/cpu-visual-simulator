088d26d81f6d0266f3ee965218e866f161527f40
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-14 15:25:21 +0200

    added getOrElse method to LocalStorage utile class

--- src/shared/util/LocalStorage.ts
+++ src/shared/util/LocalStorage.ts
@@ -13,6 +13,10 @@ class LocalStorage<T> {
 		return localStorage.getItem(String(key)) as unknown as T[typeof key] | null
 	}
 
+	public getOrElse(key: keyof T, orElse: T[typeof key]): T[typeof key] {
+		return this.isSet(key) ? (localStorage.getItem(String(key)) as unknown as T[typeof key] | null) : orElse
+	}
+
 	public isSet(key: keyof T): boolean {
 		return localStorage.getItem(String(key)) !== null
 	}
