8c2c68d06983276226732ce4aec17681c38a5535
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-14 19:27:44 +0200

    added language select to manual page

--- src/manual/components/Header.svelte
+++ src/manual/components/Header.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import LanguageSelect from "../../shared/components/LanguageSelect.svelte"
+	import { language } from "../stores/settings"
 	import text from "../stores/text"
 </script>
 
 <header class="px-7 py-4 border-b border-gray-500 flex items-center justify-between">
 	<h1 class="text-4xl font-bold text-left">{$text.title}</h1>
+	<LanguageSelect bind:value={$language} />
 </header>
