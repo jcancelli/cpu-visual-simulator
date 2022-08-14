3deef97dfae6542ae2fedd375b6ccefa644bad46
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-14 15:13:16 +0200

    added LocalStorage util class

---
+++ src/shared/util/LocalStorage.ts
@@ -0,0 +1,19 @@
+class LocalStorage<T> {
+	public readonly namespace: string
+
+	constructor(namespace: string) {
+		this.namespace = namespace
+	}
+
+	public set(key: keyof T, value: T[typeof key]): void {
+		localStorage.setItem(`${this.namespace}.${String(key)}`, value.toString())
+	}
+
+	public get(key: keyof T): T[typeof key] | null {
+		return localStorage.getItem(String(key)) as unknown as T[typeof key] | null
+	}
+
+	public isSet(key: keyof T): boolean {
+		return localStorage.getItem(String(key)) !== null
+	}
+}
