38ad2da000998c751fddfd811c5fee9318e21f86
jcancelli <jonathan.cancelli.46@gmail.com>
2022-08-27 09:52:52 +0200

    fixed single byte ALU1

--- src/app/execution/actions/cpu/state/SetALU2.ts
+++ src/app/execution/actions/cpu/state/SetALU2.ts
@@ -2,6 +2,7 @@ import { get } from "svelte/store"
 import { mux_alu_data_bus } from "../../../../store/busses"
 import { cpu } from "../../../../store/components"
 import cpuStore from "../../../../store/cpu"
+import BinaryValue from "../../../../util/BinaryValue"
 import CpuAction from "../CpuAction"
 
 export default class SetALU2 extends CpuAction {
@@ -11,7 +12,7 @@ export default class SetALU2 extends CpuAction {
 	}
 
 	protected async action(): Promise<any> {
-		cpuStore.alu2.set(get(mux_alu_data_bus))
+		cpuStore.alu2.set(new BinaryValue(16, get(mux_alu_data_bus).signed()))
 		await get(cpu).flash("ALU:2")
 	}
 }
