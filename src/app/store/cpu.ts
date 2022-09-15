82b4bed080f8449808eba062c5ec2d19b0d3a3c5
jcancelli <jonathan.cancelli.46@gmail.com>
2022-09-15 21:06:35 +0200

    removed unused stores

--- src/app/store/cpu.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from "../util/customStores"
-import Cpu from "../model/Cpu"
-
-const cpu = writable<Cpu>()
-
-export default cpu
--- src/app/store/symbolTable.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from "../util/customStores"
-import SymbolTable from "../model/SymbolTable"
-
-const symbolTable = writable<SymbolTable>()
-
-export default symbolTable
