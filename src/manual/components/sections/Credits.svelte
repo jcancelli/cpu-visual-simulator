d794ffff779c6b791ce9a38ef60d8f71d3da402a
jcancelli <jonathan.cancelli.46@gmail.com>
2022-09-18 18:52:42 +0200

    added links to research papers

--- src/manual/components/sections/Credits.svelte
+++ src/manual/components/sections/Credits.svelte
@@ -9,7 +9,11 @@
 	{/each}
 	<p class="mb-2">
 		[1]
-		<a href="" target="_blank" class="text-blue-700 hover:underline">
+		<a
+			href="https://www.ppig.org/files/2021-PPIG-32nd-DC-cortinovis.pdf"
+			target="_blank"
+			class="text-blue-700 hover:underline"
+		>
 			Cortinovis, R., and Rajan, R. (2022) Evaluating and improving the Educational CPU Visual Simulator: a
 			sustainable Open Pedagogy approach. Proceedings of the 33rd Annual Workshop of the Psychology of
 			Programming Interest Group (PPIG), Milton Keynes, United Kingdom.
@@ -17,7 +21,11 @@
 	</p>
 	<p>
 		[2]
-		<a href="" target="_blank" class="text-blue-700 hover:underline">
+		<a
+			href="resources/papers/evaluating_and_improving_the_educational_cpu_visual_simulator_a_sustainable_open_pedagogy_approach.pdf"
+			target="_blank"
+			class="text-blue-700 hover:underline"
+		>
 			Cortinovis, R. (2021) An Educational CPU Visual Simulator. Proceedings of the 32nd Annual Workshop of
 			the Psychology of Programming Interest Group (PPIG), York, United Kingdom.
 		</a>
