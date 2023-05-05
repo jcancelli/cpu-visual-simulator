0d6ee7141d8b2fb8d450d7f245c111daec3d427c
jcancelli <jonathan.cancelli.46@gmail.com>
2023-05-05 19:47:07 +0200

    Moved examples menu item to its own component

--- src/app/components/menu/Menu.svelte
+++ src/app/components/menu/Menu.svelte
@@ -1,44 +1,10 @@
 <script lang="ts">
-	import { messageFeedStore } from "../../store/state"
-	import text from "../../store/text"
-	import { ramStore, symbolTableStore } from "../../store/state"
-	import logger, { LogCategory } from "../../util/logger"
-	import { parseProgram } from "../../util/programParser"
-	import { Menu, MenuButton, MenuItems, MenuItem } from "@rgossiaux/svelte-headlessui"
-	import MenuItemIcon from "./MenuItem.svelte"
 	import Copyright from "./items/Copyright.svelte"
 	import Settings from "./items/Settings.svelte"
 	import Save from "./items/Save.svelte"
 	import Load from "./items/Load.svelte"
 	import Manual from "./items/Manual.svelte"
-
-	const examples = [
-		{
-			id: "if_then_else",
-			url: "resources/examples/if_then_else.cpuvs"
-		},
-		{
-			id: "while_do",
-			url: "resources/examples/while_do.cpuvs"
-		},
-		{
-			id: "array_sum",
-			url: "resources/examples/array_sum.cpuvs"
-		}
-	] as const
-
-	async function loadExample(exampleUrl: string): Promise<void> {
-		try {
-			const example = await fetch(exampleUrl).then(res => res.text())
-			const program = parseProgram(example)
-			symbolTableStore.get().import(program.symbolTable)
-			ramStore.get().import(program.ram)
-			logger.debug(`Example ${exampleUrl} loaded`, LogCategory.USER_INPUT)
-		} catch (error) {
-			logger.handled_error(error.message, LogCategory.USER_INPUT)
-			$messageFeedStore.error(error.message)
-		}
-	}
+	import Examples from "./items/Examples.svelte"
 </script>
 
 <div class="absolute top-0 right-0 p-2 flex flex-row-reverse gap-2 z-[4]">
@@ -46,41 +12,6 @@
 	<Settings />
 	<Save />
 	<Load />
-	<Menu>
-		<MenuButton>
-			<MenuItemIcon
-				text={$text.menu.buttons.examples.text}
-				title={$text.menu.buttons.examples.title}
-				icon="examples"
-			/>
-		</MenuButton>
-		<MenuItems
-			class="absolute flex flex-col mt-1 bg-neutral-500 border-2 border-neutral-800 shadow-lg rounded-md"
-		>
-			{#each examples as example}
-				<MenuItem
-					class="
-						bg-neutral-500
-						text-gray-200
-						text-sm
-						py-1
-						px-2
-						border-b-2
-						border-neutral-700
-						first:rounded-t-md
-						last:border-0
-						last:rounded-b-md
-						hover:brightness-[.93]
-						active:brightness-[.80]
-					"
-					title={$text.menu.buttons.examples.examples[example.id].title}
-				>
-					<button class="text-base leading-none" on:click={() => loadExample(example.url)}>
-						{$text.menu.buttons.examples.examples[example.id].text}
-					</button>
-				</MenuItem>
-			{/each}
-		</MenuItems>
-	</Menu>
+	<Examples />
 	<Manual />
 </div>
