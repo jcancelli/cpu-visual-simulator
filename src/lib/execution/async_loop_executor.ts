1fee3c7afcdf1511dc8dae4b98fa17d8a8448d4c
jcancelli <jonathan.cancelli.46@gmail.com>
2024-10-09 22:44:35 +0200

    Async loop

---
+++ src/lib/execution/async_loop_executor.ts
@@ -0,0 +1,49 @@
+import type AsyncLoop from "./async_loop"
+
+/** Queue a macro task for execution on the stack */
+const queueTask = setInterval
+
+/** Allows to execute/pause/resume a loop without hogging the execution stack. */
+export default class AsyncLoopExecutor {
+	
+	private loop: AsyncLoop
+	private _isRunning: boolean
+
+	constructor(loop: AsyncLoop) {
+		if (!loop) {
+			throw new Error(`null or undefined loop`)
+		}
+		this.loop = loop
+		this._isRunning = false
+	}
+
+	/** Start loop execution */
+	start() {
+		if (this._isRunning) {
+			return
+		}
+		queueTask(this.cycle.bind(this))
+	}
+
+	/** Pause/stops loop execution */
+	stop() {
+		this._isRunning = false
+	}
+
+	/** Wether the loop is currently running or not */
+	isRunning(): boolean {
+		return this._isRunning
+	}
+
+	/** Execute one cycle of the loop */
+	private async cycle(): Promise<void> {
+		const keepRunning = await this.loop.cycle()
+		
+		if (this._isRunning && keepRunning) {
+			queueTask(this.cycle.bind(this))
+		} else {
+			this._isRunning = false
+		}
+	}
+
+}
