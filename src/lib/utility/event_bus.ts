3060faf35d249dad619333fbcde4acb58a948eb4
jcancelli <jonathan.cancelli.46@gmail.com>
2024-10-20 23:46:54 +0200

    Add event bus utility class

---
+++ src/lib/utility/event_bus.ts
@@ -0,0 +1,40 @@
+/** Event bus that manage listeners to different events */
+export default class EventBus<Events extends string, EventsTypes extends TypesMap<Events>> {
+	private listeners: ListenersMap<Events, EventsTypes> = {}
+
+	/** Subscribe a new listener to the specified event */
+	addListener(event: Events, listener: EventListener<EventsTypes[typeof event]>) {
+		if (!this.listeners[event]) {
+			this.listeners[event] = new Set()
+		}
+		this.listeners[event].add(listener)
+	}
+
+	/** Unsubscribe a new listener to the specified event */
+	removeListener(event: Events, listener: EventListener<EventsTypes[typeof event]>) {
+		if (!this.listeners[event]) {
+			return
+		}
+		this.listeners[event].delete(listener)
+	}
+
+	/** Notify all listeners subscribed to the provided event */
+	notify(event: Events, eventData: EventsTypes[typeof event]) {
+		if (!this.listeners[event]) {
+			return
+		}
+		this.listeners[event].forEach(listener => listener(eventData))
+	}
+}
+
+/** A function that is executed when an event it was subscribed to happens */
+export type EventListener<EventDataType> = (data: EventDataType) => void
+
+/** Utility type to map an event key to the event data type */
+export type TypesMap<Keys extends string> = {
+	[key in Keys]: any
+}
+
+type ListenersMap<Keys extends string, Types extends TypesMap<Keys>> = {
+	[key in Keys]?: Set<EventListener<Types[key]>>
+}
