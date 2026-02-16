542587d4f16e8ad293dfdd06ceadb45f60e5de60
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-16 16:14:56 +0100

    Implement memory state

---
+++ src/lib/memory.svelte.ts
@@ -0,0 +1,196 @@
+import {
+	checkI16Throw,
+	checkI8Throw,
+	checkU16Throw,
+	checkU8Throw,
+	i16MSB,
+	isValidU8,
+	u16LSB,
+	u16MSB,
+	u8,
+} from "./integer"
+
+/** The lowest valid address */
+export const MIN_ADDRESS = 0
+/** The highest valid address for a byte */
+export const MAX_ADDRESS = 255
+/** The highest valid address for a word */
+export const MAX_WORD_ADDRESS = 254
+/** Size of a word in bytes */
+export const WORD_ALIGN = 2
+/** Size in bytes of the memory */
+export const MEMORY_SIZE_BYTES = 256
+/** Size in words of the memory */
+export const MEMORY_SIZE_WORDS = MEMORY_SIZE_BYTES / WORD_ALIGN
+
+/** Return wether or not the provided value is a valid address for a byte */
+export function isValidByteAddress(address: number): boolean {
+	return isValidU8(address)
+}
+
+/** Return wether or not the provided value is a valid address for a word */
+export function isValidWordAddress(address: number): boolean {
+	return isValidU8(address) && (address & 1) === 0
+}
+
+/** Shortcut to check if an address can point to a byte.
+ * @throws {InvalidByteAddressError} when the address is not a valid byte address */
+export function checkByteAddressThrow(address: number): void {
+	if (!isValidByteAddress(address)) {
+		throw new InvalidByteAddressError(address)
+	}
+}
+
+/** Shortcut to check if an address can point to a word.
+ * @throws {InvalidWordAddressError} when the address is not a valid word address */
+export function checkWordAddressThrow(address: number): void {
+	if (!isValidWordAddress(address)) {
+		throw new InvalidWordAddressError(address)
+	}
+}
+
+/** State of the memory */
+export default class Memory {
+	/** Readonly state containing the memory's contents. */
+	public readonly bytes: number[]
+	/** Writable state containing the memory's contents. */
+	private _bytes: number[]
+
+	constructor() {
+		this._bytes = $state(new Array(MEMORY_SIZE_BYTES).fill(0))
+		this.bytes = $derived(this._bytes)
+	}
+
+	/** Set all bytes to 0. */
+	clear(): void {
+		for (let address = MIN_ADDRESS; address <= MAX_ADDRESS; address += 1) {
+			this._bytes[address] = 0
+		}
+	}
+
+	/** Write the specified 8-bit unsigned integer at the specified address.
+	 * @throws {InvalidByteAddressError} if the address is not a valid byte address.
+	 * @throws {InvalidU8Error} if the value is not a valid 8-bit unsigned integer. */
+	writeU8(address: number, value: number): void {
+		checkByteAddressThrow(address)
+		checkU8Throw(value)
+		this._bytes[address] = value
+	}
+
+	/** Write the specified 8-bit signed integer at the specified address.
+	 * @throws {InvalidByteAddressError} if the address is not a valid byte address.
+	 * @throws {InvalidI8Error} if the value is not a valid 8-bit signed integer. */
+	writeI8(address: number, value: number): void {
+		checkByteAddressThrow(address)
+		checkI8Throw(value)
+		this._bytes[address] = u8(value)
+	}
+
+	/** Write the specified 16-bit unsigned integer at the specified address.
+	 * @throws {InvalidWordAddressError} if the address is not a valid word address.
+	 * @throws {InvalidU16Error} if the value is not a valid 16-bit unsigned integer. */
+	writeU16(address: number, value: number): void {
+		checkWordAddressThrow(address)
+		checkU16Throw(value)
+		const msb = u16MSB(value)
+		const lsb = u16LSB(value)
+		this._bytes[address] = msb
+		this._bytes[address + 1] = lsb
+	}
+
+	/** Write the specified 16-bit signed integer at the specified address.
+	 * @throws {InvalidWordAddressError} if the address is not a valid word address.
+	 * @throws {InvalidI16Error} if the value is not a valid 16-bit signed integer. */
+	writeI16(address: number, value: number): void {
+		checkWordAddressThrow(address)
+		checkI16Throw(value)
+		const msb = i16MSB(value)
+		const lsb = u16LSB(value)
+		this._bytes[address] = msb
+		this._bytes[address + 1] = lsb
+	}
+
+	/** Shift down by {@link WORD_ALIGN} all bytes from {@link msbAddress} to {@link MAX_WORD_ADDRESS} - 1.
+	 * {@link MAX_WORD_ADDRESS} and {@link MAX_WORD_ADDRESS} + 1 are overwritten.
+	 * {@link msbAddress} and {@link msbAddress} + 1 are set to 0.
+	 * @throws {InvalidWordAddressError} if the address is not a valid word address. */
+	shiftWordsDownFromAddress(msbAddress: number): void {
+		checkWordAddressThrow(msbAddress)
+		const lsbAddress = msbAddress + 1
+		for (let newAddress = MAX_ADDRESS; newAddress > lsbAddress; newAddress -= 1) {
+			const oldAddress = newAddress - WORD_ALIGN
+			this._bytes[newAddress] = this._bytes[oldAddress]
+		}
+		this._bytes[msbAddress] = 0
+		this._bytes[lsbAddress] = 0
+	}
+
+	/** Shift down by {@link WORD_ALIGN} all bytes from {@link MIN_ADDRESS} to {@link msbAddress} - 1.
+	 * {@link msbAddress} and {@link msbAddress} + 1 are overwritten.
+	 * {@link MIN_ADDRESS} and {@link MIN_ADDRESS} + 1 are set to 0.
+	 * @throws {InvalidWordAddressError} if the address is not a valid word address. */
+	shiftWordsDownToAddress(msbAddress: number): void {
+		checkWordAddressThrow(msbAddress)
+		const lsbAddress = msbAddress + 1
+		for (let newAddress = lsbAddress; newAddress > MIN_ADDRESS + 1; newAddress -= 1) {
+			const oldAddress = newAddress - WORD_ALIGN
+			this._bytes[newAddress] = this._bytes[oldAddress]
+		}
+		this._bytes[MIN_ADDRESS] = 0
+		this._bytes[MIN_ADDRESS + 1] = 0
+	}
+
+	/** Shift up by {@link WORD_ALIGN} all bytes from {@link MAX_ADDRESS} to {@link msbAddress} + {@link WORD_ALIGN}.
+	 * {@link msbAddress} and {@link msbAddress} + 1 are overwritten.
+	 * {@link MAX_WORD_ADDRESS} and {@link MAX_WORD_ADDRESS} + 1 are set to 0.
+	 * @throws {InvalidWordAddressError} if the address is not a valid word address. */
+	shiftWordsUpFromAddress(msbAddress: number): void {
+		checkWordAddressThrow(msbAddress)
+		for (let newAddress = msbAddress; newAddress < MAX_WORD_ADDRESS; newAddress += 1) {
+			const oldAddress = newAddress + WORD_ALIGN
+			this._bytes[newAddress] = this._bytes[oldAddress]
+		}
+		this._bytes[MAX_WORD_ADDRESS] = 0
+		this._bytes[MAX_WORD_ADDRESS + 1] = 0
+	}
+
+	/** Shift up by {@link WORD_ALIGN} all bytes from {@link msbAddress} + 1 to {@link MIN_ADDRESS} + {@link WORD_ALIGN}.
+	 * {@link MIN_ADDRESS} and {@link MIN_ADDRESS} + 1 are overwritten.
+	 * {@link msbAddress} and {@link msbAddress} + 1 are set to 0.
+	 * @throws {InvalidWordAddressError} if the address is not a valid word address. */
+	shiftWordsUpToAddress(msbAddress: number): void {
+		checkWordAddressThrow(msbAddress)
+		const lsbAddress = msbAddress + 1
+		for (let newAddress = MIN_ADDRESS; newAddress < msbAddress; newAddress += 1) {
+			const oldAddress = newAddress + WORD_ALIGN
+			this._bytes[newAddress] = this._bytes[oldAddress]
+		}
+		this._bytes[msbAddress] = 0
+		this._bytes[lsbAddress] = 0
+	}
+}
+
+/** Base class for errors regarding an invalid address */
+export abstract class InvalidAddressError extends Error {
+	/** The address that caused the error. */
+	public readonly address: number
+
+	constructor(address: number, message: string) {
+		super(message)
+		this.address = address
+	}
+}
+
+/** Error regarding an address that cannot point to a byte */
+export class InvalidByteAddressError extends InvalidAddressError {
+	constructor(address: number) {
+		super(address, `Invalid byte address: ${address}`)
+	}
+}
+
+/** Error regarding an address that cannot point to a word (16-bit value) */
+export class InvalidWordAddressError extends InvalidAddressError {
+	constructor(address: number) {
+		super(address, `Invalid word address: ${address}`)
+	}
+}
