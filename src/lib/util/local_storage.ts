c981296b40c353fae4526d00c14e67c818aa6bf1
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-17 11:29:16 +0100

    Add local storage utilities

---
+++ src/lib/util/local_storage.ts
@@ -0,0 +1,35 @@
+export function setNumber(key: string, num: number): void {
+	localStorage.setItem(key, num.toString())
+}
+
+export function getNumber(key: string): number | null {
+	const str = localStorage.getItem(key)
+	if (str === null) {
+		return null
+	}
+	return +str
+}
+
+export function setBool(key: string, bool: boolean): void {
+	localStorage.setItem(key, bool ? "true" : "false")
+}
+
+export function getBool(key: string): boolean | null {
+	const str = localStorage.getItem(key)
+	if (str === null) {
+		return null
+	}
+	return str === "true"
+}
+
+export function setJSON<T>(key: string, obj: T): void {
+	localStorage.setItem(key, JSON.stringify(obj))
+}
+
+export function getJSON<T>(key: string): T | null {
+	const str = localStorage.getItem(key)
+	if (str === null) {
+		return null
+	}
+	return JSON.parse(str)
+}
