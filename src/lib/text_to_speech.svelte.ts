bef97b787c688ed026246de1552957a20ff9792a
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-19 14:13:49 +0100

    Rework text-to-speech system

--- src/lib/text_to_speech.svelte.ts
+++ src/lib/text_to_speech.svelte.ts
@@ -1,11 +1,13 @@
-import { getLocale, type Locale } from "./paraglide/runtime"
-import { getString, setString } from "./util/local_storage"
-
-/** True if text to speech is not available on this browser */
-export const TEXT_TO_SPEECH_UNAVAILABLE = window.speechSynthesis === undefined
+import { ActionType } from "./execution/action"
+import type { ActionHandlerMapping, ActionPerformer } from "./execution/task_system"
+import { type Locale } from "./paraglide/runtime"
+import { todo } from "./util/development"
 
 /** Wrapper for {@link window.speechSynthesis} */
-export default class TextToSpeech {
+export default class TextToSpeech implements ActionPerformer {
+	public readonly isAvailable: Promise<boolean>
+	/** Wether text-to-speech is enabled or not */
+	private _isEnabled: boolean
 	/** The utterance that is currently being read. */
 	private utterance: SpeechSynthesisUtterance | null
 	/** Private mutable state containing the promise that resolves when TTS has finished reading. */
@@ -15,33 +17,59 @@ export default class TextToSpeech {
 	/** Private mutable state containing the currently selected voice. */
 	private _voice: SpeechSynthesisVoice | null
 
-	constructor() {
+	constructor(locale: Locale, initVoiceURI?: string) {
+		this.isAvailable = new Promise<boolean>(resolve => {
+			if (window.speechSynthesis === undefined) {
+				resolve(false)
+				return
+			}
+			this._voices = getLocalizedVoices(locale)
+			if (this._voices.length === 0) {
+				// If the voice list is still empty after 10 seconds, give up
+				let giveUp = false
+				const timeoutID = setTimeout(() => {
+					giveUp = true
+					resolve(false)
+				}, 10_000)
+
+				window.speechSynthesis.onvoiceschanged = () => {
+					// Do not resolve promise if the timeout already resolved it
+					if (giveUp) {
+						return
+					}
+					this._voices = getLocalizedVoices(locale)
+					if (this._voices.length > 0) {
+						clearTimeout(timeoutID)
+					}
+					window.speechSynthesis.onvoiceschanged = () => {
+						this._voices = getLocalizedVoices(locale)
+					}
+					resolve(true)
+				}
+				return
+			}
+			resolve(true)
+		}).then(isAvailable => {
+			if (isAvailable) {
+				this._voice =
+					this._voices.find(voice => voice.voiceURI === initVoiceURI) // Try init voice
+					?? this._voices.find(voice => voice.default) // Fallback to the one marked as default by the browser
+					?? this._voices[0] // Fallback to the first one
+			}
+			return isAvailable
+		})
+		this._isEnabled = $state(false)
 		this.utterance = null
 		this._speechPromise = $state(null)
 		this._voices = $state([])
 		this._voice = $state(null)
-
-		if (!TEXT_TO_SPEECH_UNAVAILABLE) {
-			const locale = getLocale()
-			this._voices = getLocalizedVoices(locale)
-			window.speechSynthesis.onvoiceschanged = () => {
-				this._voices = getLocalizedVoices(locale)
-				this.setVoiceToDefault(locale)
-			}
-			this.setVoiceToDefault(locale)
-			// Update the current utterance voice when _voice changes
-			$effect(() => {
-				if (this._voice !== null && this.utterance !== null) {
-					this.utterance.voice = this._voice
-				}
-			})
-		}
 	}
+
 	/** Read the provided text at the specified rate after the previous sentence (if any) has ended.
 	 * If TTS is not supported by the browser, nothing happens.
 	 * @returns A promise that is resolved when the utterance has ended. */
-	async speak(text: string, rate: number = 1): Promise<void> {
-		if (TEXT_TO_SPEECH_UNAVAILABLE) {
+	async read(text: string, rate: number = 1): Promise<void> {
+		if (!(await this.isAvailable) || !this._isEnabled) {
 			return
 		}
 		await this._speechPromise
@@ -55,10 +83,9 @@ export default class TextToSpeech {
 		return this._speechPromise
 	}
 
-	/** Stop speaking.
-	 * If TTS is not supported by the browser, nothing happens. */
-	stopSpeaking(): void {
-		if (TEXT_TO_SPEECH_UNAVAILABLE) {
+	/** Stop reading */
+	async stopReading(): Promise<void> {
+		if (!(await this.isAvailable)) {
 			return
 		}
 		window.speechSynthesis.cancel()
@@ -66,9 +93,25 @@ export default class TextToSpeech {
 		this._speechPromise = null
 	}
 
-	/** Readonly state containing the promise that resolves when TTS has finished reading. */
-	get speechPromise(): Promise<void> | null {
-		return this._speechPromise
+	/** Await fot TTS to stop reading */
+	async awaitReadingEnd(): Promise<void> {
+		await this._speechPromise
+	}
+
+	/** Wether text-to-speech is enabled or not */
+	get isEnabled(): boolean {
+		return this._isEnabled
+	}
+
+	/** Wether text-to-speech is enabled or not */
+	set isEnabled(enabled: boolean) {
+		if (this.isEnabled === enabled) {
+			return
+		}
+		if (!enabled) {
+			this.stopReading()
+		}
+		this.isEnabled = enabled
 	}
 
 	/** Readonly state containing all the currently available voices for the current locale. */
@@ -83,55 +126,33 @@ export default class TextToSpeech {
 
 	/** Mutable state containing the currently selected voice. */
 	set voice(voice: SpeechSynthesisVoice) {
-		if (TEXT_TO_SPEECH_UNAVAILABLE) {
-			return
-		}
 		if (voice === this._voice) {
 			return
 		}
 		this._voice = voice
-		storeUserVoicePreference(voice)
 	}
 
-	/** Set the current voice for the specified (or current) locale to the default one.
-	 * The lookup order is: user preference -> voice marked as "default" -> the first available voice -> null */
-	private setVoiceToDefault(locale: Locale = getLocale()): void {
-		const userPreference = retrieveUserVoicePreference(this._voices, locale)
-		if (userPreference !== null) {
-			this._voice = userPreference
-			return
-		}
-		this._voice = this._voices.find(voice => voice.default) ?? this._voices[0] ?? null
-		// WARN: Haven't tested it yet, but it could be possible that synth.getVoices() starts empty
-		// or without the user prefered voice and only later gets updated and fires synth.onvoiceschanged.
-		// If this happens, this storeUserVoicePreference call would always overwrite the user preference with
-		// the fallback value
-		storeUserVoicePreference(this._voice, locale)
+	getActionHandlersMappings(): ActionHandlerMapping[] {
+		return [
+			{
+				action: ActionType.TEXT_TO_SPEECH_READ,
+				handler: async () => {
+					todo("Define text-to-speech localized strings") // TODO: Define text to speech localized strings
+				},
+			},
+			{
+				action: ActionType.AWAIT_TEXT_TO_SPEECH_END,
+				handler: async () => {
+					await this._speechPromise
+					return { actionWasHandled: true }
+				},
+			},
+		]
 	}
-}
-
-/** Store the specified {@link SpeechSynthesisVoice} as the user preference for the specified (or current) locale. */
-function storeUserVoicePreference(voice: SpeechSynthesisVoice, locale: Locale = getLocale()): void {
-	setString(`tts_voice_preference_${locale}`, voice.name)
-}
-
-/** @returns The {@link SpeechSynthesisVoice} prefered by the user for the specified (or current) locale or null. */
-function retrieveUserVoicePreference(
-	voices: SpeechSynthesisVoice[],
-	locale: Locale = getLocale(),
-): SpeechSynthesisVoice | null {
-	const voiceName = getString(`tts_voice_preference_${locale}`)
-	if (voiceName === null) {
-		return null
-	}
-	return voices.find(voice => voice.name === voiceName) ?? null
 }
 
 /** @returns All the {@link SpeechSynthesisVoice} supported by the browser that match the provided locale.
  * If TTS is not supported, an empty array is returned. */
 function getLocalizedVoices(locale: Locale): SpeechSynthesisVoice[] {
-	if (TEXT_TO_SPEECH_UNAVAILABLE) {
-		return []
-	}
 	return window.speechSynthesis.getVoices().filter(voice => voice.lang.startsWith(locale))
 }
