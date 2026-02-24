c842c116f93e9f8aca21b1593d2ad3ee408201ec
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-24 11:01:48 +0100

    Rename 'enums' directory to 'types' and move integer into it

--- src/lib/types/integer.ts
+++ src/lib/types/integer.ts
@@ -161,38 +161,3 @@ export function joinU8ToI16(msb: U8, lsb: U8): I16 {
 export function joinU8ToU16(msb: U8, lsb: U8): U16 {
 	return ((msb << 8) | lsb) as U16
 }
-
-/** Base class for errors regarding numeric values out of a specific n-bits signed/unsigned range */
-export abstract class IntegerOutOfRangeError extends Error {
-	constructor(bits: 8 | 16, signed: boolean, value: number) {
-		super(`value: ${value} out of ${bits}-bit ${signed ? "signed" : "unsigned"} range`)
-	}
-}
-
-/** Error regarding numeric values out of 8-bit unsigned integer valid range */
-export class InvalidU8Error extends IntegerOutOfRangeError {
-	constructor(value: number) {
-		super(8, false, value)
-	}
-}
-
-/** Error regarding numeric values out of 8-bit signed integer valid range */
-export class InvalidI8Error extends IntegerOutOfRangeError {
-	constructor(value: number) {
-		super(8, true, value)
-	}
-}
-
-/** Error regarding numeric values out of 16-bit unsigned integer valid range */
-export class InvalidU16Error extends IntegerOutOfRangeError {
-	constructor(value: number) {
-		super(16, false, value)
-	}
-}
-
-/** Error regarding numeric values out of 16-bit signed integer valid range */
-export class InvalidI16Error extends IntegerOutOfRangeError {
-	constructor(value: number) {
-		super(16, true, value)
-	}
-}
