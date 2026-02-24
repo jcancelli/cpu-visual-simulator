6c26e18253b7da9b4712c16c5904c4ac02f30196
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-24 11:13:05 +0100

    Move opcode module into 'types' and 'errors' directories

--- src/lib/types/opcode.ts
+++ src/lib/types/opcode.ts
@@ -1,3 +1,10 @@
+import {
+	ImmediateFlagNotAllowedError,
+	InvalidNumericOpcodeError,
+	InvalidSymbolicOpcodeError,
+	OperandNotAllowedError,
+	OperandRequiredError,
+} from "$lib/errors/opcode"
 import type { Byte } from "./integer"
 
 /** Immediate flag bit set to 1, all other bits set to 0 */
@@ -396,62 +403,4 @@ export function opcodeDoesNotAllowImmediateOperand(
 	opcode: Opcode,
 ): opcode is OpcodeWithoutImmediate {
 	return !opcode.takesImmediate
-}
-
-/** Base class for errors regarding {@link Opcode} */
-export abstract class OpcodeError extends Error {}
-
-/** Error regarding a number that does not match any valid {@link OpcodeNumeric} */
-export class InvalidNumericOpcodeError extends OpcodeError {
-	/** The value that caused the error */
-	public readonly invalidValue: number
-
-	constructor(invalidValue: number) {
-		super(`"${invalidValue}" is not a valid numeric opcode value`)
-		this.invalidValue = invalidValue
-	}
-}
-
-/** Error regarding a string that does not match any valid {@link OpcodeSymbolic} */
-export class InvalidSymbolicOpcodeError extends OpcodeError {
-	/** The value that caused the error */
-	public readonly invalidValue: string
-
-	constructor(invalidValue: string) {
-		super(`"${invalidValue}" is not a valid opcode`)
-		this.invalidValue = invalidValue
-	}
-}
-
-/** Error regarding an opcode that does not take operands but an operand was found */
-export class OperandNotAllowedError extends OpcodeError {
-	/** The opcode that caused the error */
-	public readonly opcode: OpcodeWithoutOperand
-
-	constructor(opcode: OpcodeWithoutOperand) {
-		super(`Opcode ${opcode.symbolic} does not take an operand`)
-		this.opcode = opcode
-	}
-}
-
-/** Error regarding an opcode that needs an operand but no operand was found */
-export class OperandRequiredError extends OpcodeError {
-	/** The opcode that caused the error */
-	public readonly opcode: OpcodeWithOperand
-
-	constructor(opcode: OpcodeWithOperand) {
-		super(`Opcode ${opcode.symbolic} requires an operand`)
-		this.opcode = opcode
-	}
-}
-
-/** Error regarding an opcode that does not allow immediate operands but an immediate flag was found */
-export class ImmediateFlagNotAllowedError extends OpcodeError {
-	/** The opcode that caused the error */
-	public readonly opcode: OpcodeWithoutImmediate
-
-	constructor(opcode: OpcodeWithoutImmediate) {
-		super(`Opcode ${opcode.symbolic} does not allow immediate operands`)
-		this.opcode = opcode
-	}
 }
