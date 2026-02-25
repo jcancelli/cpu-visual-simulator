63b7dbaf0e3bcc6d5b3d0b2a5d65430fddc4b40d
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-25 13:09:20 +0100

    Add status word branded type and related functions

--- src/lib/types/cpu.ts
+++ src/lib/types/cpu.ts
@@ -1,4 +1,4 @@
-import { InvalidAddressingModeError, InvalidStatusWordError } from "$lib/errors/cpu"
+import { InvalidAddressingModeError } from "$lib/errors/cpu"
 
 /** From which bus the operand should be read */
 export enum Addressing {
@@ -18,29 +18,5 @@ export function isValidAddressing(value: number): value is Addressing {
 export function assertAddressing(value: number): asserts value is Addressing {
 	if (!isValidAddressing(value)) {
 		throw new InvalidAddressingModeError(value)
-	}
-}
-
-/** The bit of the status word zero flag */
-export const ZERO_FLAG_BIT = 1 << 7
-/** All the bits of the status word except the zero flag set to 1 */
-export const ZERO_FLAG_MASK = ~ZERO_FLAG_BIT >>> 0
-/** The bit of the status word negative flag */
-export const NEGATIVE_FLAG_BIT = 1 << 6
-/** All the bits of the status word except the negative flag set to 1 */
-export const NEGATIVE_FLAG_MASK = ~NEGATIVE_FLAG_BIT >>> 0
-/** All the bits of the status word set to 0 except the ones used for flags */
-export const ALL_FLAGS = ZERO_FLAG_BIT | NEGATIVE_FLAG_BIT
-
-/** Check if the provided value is a valid status word value */
-export function isStatusWord(value: number): boolean {
-	return (value & ALL_FLAGS) === value
-}
-
-/** Asserts that the provided value is a valid status word value.
- * @throws {InvalidStatusWordError} */
-export function assertStatusWord(value: number): void {
-	if (!isStatusWord(value)) {
-		throw new InvalidStatusWordError(value)
 	}
 }
---
+++ src/lib/types/status_word.ts
@@ -0,0 +1,85 @@
+import { InvalidStatusWordError } from "$lib/errors/cpu"
+import type { U8 } from "./integer"
+
+/** The bit of the status word zero flag */
+export const ZERO_FLAG_BIT = 1 << 7
+/** All the bits of the status word except the zero flag set to 1 */
+export const ZERO_FLAG_MASK = ~ZERO_FLAG_BIT >>> 0
+/** The bit of the status word negative flag */
+export const NEGATIVE_FLAG_BIT = 1 << 6
+/** All the bits of the status word except the negative flag set to 1 */
+export const NEGATIVE_FLAG_MASK = ~NEGATIVE_FLAG_BIT >>> 0
+/** All the bits of the status word set to 0 except the ones used for flags */
+export const ALL_FLAGS = ZERO_FLAG_BIT | NEGATIVE_FLAG_BIT
+
+/** A byte that stores the zero flag and negative flag */
+export type StatusWord = U8 & { __statusWord: true }
+
+/** Check if the provided value is a valid status word value */
+export function isStatusWord(value: number): value is StatusWord {
+	return (value & ALL_FLAGS) === value
+}
+
+/** Asserts that the provided value is a valid status word value.
+ * @throws {InvalidStatusWordError} */
+export function assertStatusWord(value: number): asserts value is StatusWord {
+	if (!isStatusWord(value)) {
+		throw new InvalidStatusWordError(value)
+	}
+}
+
+/** @returns The provided status word with the zero flag bit set to 1.
+ * No validation is performed on the input. */
+export function withZeroFlag(value: StatusWord): StatusWord {
+	return (value | ZERO_FLAG_BIT) as StatusWord
+}
+
+/** @returns The provided status word with the zero flag bit set to 0.
+ * No validation is performed on the input. */
+export function withoutZeroFlag(value: StatusWord): StatusWord {
+	return (value & ZERO_FLAG_MASK) as StatusWord
+}
+
+/** @returns The state of the zero flag bit in the provided value.
+ * No validation is performed on the input. */
+export function getZeroFlag(value: StatusWord): boolean {
+	return (value & ZERO_FLAG_BIT) === ZERO_FLAG_BIT
+}
+
+/** @returns The provided status word with the zero flag bit set to either 1 or 0.
+ * No validation is performed on the input. */
+export function setZeroFlag(value: StatusWord, flag: boolean): StatusWord {
+	return flag ? withZeroFlag(value) : withoutZeroFlag(value)
+}
+
+/** @returns The provided status word with the negative flag bit set to 1.
+ * No validation is performed on the input. */
+export function withNegativeFlag(value: StatusWord): StatusWord {
+	return (value | NEGATIVE_FLAG_BIT) as StatusWord
+}
+
+/** @returns The provided status word with the negative flag bit set to 0.
+ * No validation is performed on the input. */
+export function withoutNegativeFlag(value: StatusWord): StatusWord {
+	return (value & NEGATIVE_FLAG_MASK) as StatusWord
+}
+
+/** @returns The state of the negative flag bit in the provided value.
+ * No validation is performed on the input. */
+export function getNegativeFlag(value: StatusWord): boolean {
+	return (value & NEGATIVE_FLAG_BIT) === NEGATIVE_FLAG_BIT
+}
+
+/** @returns The provided status word with the negative flag bit set to either 1 or 0.
+ * No validation is performed on the input. */
+export function setNegativeFlag(value: StatusWord, flag: boolean): StatusWord {
+	return flag ? withNegativeFlag(value) : withoutNegativeFlag(value)
+}
+
+/** Encode the provided flags into a status word */
+export function encodeStatusWord(
+	zeroFlag: boolean = true,
+	negativeFlag: boolean = false,
+): StatusWord {
+	return setNegativeFlag(setZeroFlag(0 as StatusWord, zeroFlag), negativeFlag)
+}
