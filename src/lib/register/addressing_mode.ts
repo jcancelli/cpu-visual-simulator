2f58d08b96193d71ca986a877b94bb8b53512453
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-25 13:42:46 +0100

    No copy-pasterino 2

--- src/lib/register/addressing_mode.ts
+++ src/lib/register/addressing_mode.ts
@@ -4,9 +4,9 @@ import { ByteRegisterImpl, type ByteRegister } from "./register.svelte"
 
 /** Register that can store a valid memory operation value */
 export interface AddressingModeRegister extends ByteRegister {
-	/** The value of the memory operation */
+	/** The value of the addressing mode stored in this register */
 	get value(): Addressing
-	/** The value of the memory operation.
+	/** The value of the addressing mode stored in this register.
 	 * @throws {InvalidAddressingModeError} */
 	set value(value: Addressing)
 }
