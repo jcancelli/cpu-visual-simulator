aef50242aeb1d45fedf86fb88a06433a86909470
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-25 13:36:31 +0100

    Implement addressing mode register

---
+++ src/lib/register/addressing_mode.ts
@@ -0,0 +1,31 @@
+import { Addressing, assertAddressing } from "$lib/types/cpu"
+import type { U8 } from "$lib/types/integer"
+import { ByteRegisterImpl, type ByteRegister } from "./register.svelte"
+
+/** Register that can store a valid memory operation value */
+export interface AddressingModeRegister extends ByteRegister {
+	/** The value of the memory operation */
+	get value(): Addressing
+	/** The value of the memory operation.
+	 * @throws {InvalidAddressingModeError} */
+	set value(value: Addressing)
+}
+
+/** Implementation of {@link AddressingModeRegisterImpl} */
+export class AddressingModeRegisterImpl extends ByteRegisterImpl implements AddressingModeRegister {
+	constructor(initialValue: Addressing = Addressing.IMMEDIATE) {
+		super(initialValue as U8)
+	}
+
+	get value(): Addressing {
+		return this.unsigned as Addressing
+	}
+
+	set value(value: Addressing) {
+		this.unsigned = value
+	}
+
+	protected override assertValid(value: U8): void {
+		assertAddressing(value)
+	}
+}
