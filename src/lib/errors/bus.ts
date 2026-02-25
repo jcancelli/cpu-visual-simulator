5f54c6ced851c7fc14da87221e13703274b61a0c
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-25 15:16:25 +0100

    Make busid optional for bus errors

--- src/lib/errors/bus.ts
+++ src/lib/errors/bus.ts
@@ -7,10 +7,14 @@ export abstract class BusError extends CPUVSError {}
 /** Error regarding a send signal operation on a {@link Bus} that is already carrying a signal */
 export class SignalConflictError extends BusError {
 	/** The bus regarding the conflict of signals */
-	public readonly busID: BusID
+	public readonly busID?: BusID
 
-	constructor(busID: BusID) {
-		super(`Multiple signals being sent on bus "${BusID[busID]}"`)
+	constructor(busID?: BusID) {
+		if (busID !== undefined) {
+			super(`Multiple signals are being sent on bus "${BusID[busID]}"`)
+		} else {
+			super("Multiple signals are being sent on the same bus")
+		}
 		this.busID = busID
 	}
 }
@@ -18,10 +22,14 @@ export class SignalConflictError extends BusError {
 /** Error regarding a read/end signal operation on a {@link Bus} that is not carrying a signal */
 export class NoSignalError extends BusError {
 	/** The bus with no signal */
-	public readonly busID: BusID
+	public readonly busID?: BusID
 
-	constructor(busID: BusID) {
-		super(`Tring to end/read signal from bus "${BusID[busID]}" that has no signal`)
+	constructor(busID?: BusID) {
+		if (busID !== undefined) {
+			super(`Tring to end/read signal from bus "${BusID[busID]}" that has no signal`)
+		} else {
+			super("Tring to end/read a signal from a bus with no signal")
+		}
 		this.busID = busID
 	}
 }
