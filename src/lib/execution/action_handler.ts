e586c73c7131bd37309876fc23d4441cade99fb9
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-25 17:38:54 +0100

    Rework action handler interface

--- src/lib/execution/action_handler.ts
+++ src/lib/execution/action_handler.ts
@@ -1,13 +1,15 @@
+import type { ActionType } from "$lib/types/action"
 import type { Action, Task } from "./task"
 
-/** Function that implements an {@link Action} execution.
- * @param action The action that needs to be executed
- * @param submitTasks A function that allows the handler to submit more tasks for execution
- * @returns Wether the action was handled or not */
-export type ActionHandler<T extends Action> = (
-	action: T,
-	submitTasks: SubmitTasksFunction,
-) => Promise<boolean> | boolean
+/** Implementation of an {@link Action} behavior */
+export interface ActionHandler<TAction extends Action> {
+	/** The action type handled by this handler */
+	get actionType(): ActionType
+	/** Function that implements an {@link Action} execution.
+	 * @param action The action that needs to be executed
+	 * @param submitTasks A function that allows the handler to submit more tasks for execution */
+	handle(action: TAction, submitTasks: SubmitTasksFunction): Promise<void> | void
+}
 
-/** Function passed to the {@link ActionHandler} that allows the handler to submit new tasks for execution */
+/** Function passed to an {@link ActionHandler.handle} that allows it to submit more tasks for execution */
 export type SubmitTasksFunction = (...tasks: Task[]) => void
---
+++ src/lib/execution/handlers/bus.ts
@@ -0,0 +1,80 @@
+import { assertSignal, type Bus, type BusSizeBits } from "$lib/bus/bus.svelte"
+import type { Register, RegisterSizeBits } from "$lib/register/register.svelte"
+import { ActionType } from "$lib/types/action"
+import type { BusID, RegisterID } from "$lib/types/bus"
+import { assert } from "$lib/util/development"
+import type { ActionHandler } from "../action_handler"
+import type { EndSignalBusAction, ReadSignalBusAction, SendSignalBusAction } from "../actions/bus"
+
+/** Busses indexed by their ID */
+export type BusByID = {
+	[ID in BusID]: Bus<BusSizeBits>
+}
+
+/** Registers indexed by their ID */
+export type RegisterByID = {
+	[ID in RegisterID]: Register<RegisterSizeBits>
+}
+
+/** {@link ActionHandler} for {@link SendSignalBusAction} */
+export class SendSignalActionHandler implements ActionHandler<SendSignalBusAction> {
+	private readonly busses: BusByID
+	private readonly registers: RegisterByID
+
+	constructor(busses: BusByID, registers: RegisterByID) {
+		this.busses = busses
+		this.registers = registers
+	}
+
+	get actionType(): ActionType {
+		return ActionType.SEND_SIGNAL
+	}
+
+	handle(action: SendSignalBusAction): void {
+		const register = this.registers[action.from]
+		const bus = this.busses[action.onBus]
+		assert(register.sizeBits <= bus.sizeBits)
+		bus.sendSignalUnsigned(register.unsigned)
+	}
+}
+
+/** {@link ActionHandler} for {@link EndSignalBusAction} */
+export class EndSignalActionHandler implements ActionHandler<EndSignalBusAction> {
+	private readonly busses: BusByID
+
+	constructor(busses: BusByID) {
+		this.busses = busses
+	}
+
+	get actionType(): ActionType {
+		return ActionType.END_SIGNAL
+	}
+
+	handle(action: EndSignalBusAction): void {
+		const bus = this.busses[action.bus]
+		bus.endSignal()
+	}
+}
+
+/** {@link ActionHandler} for {@link ReadSignalBusAction} */
+export class ReadSignalActionHandler implements ActionHandler<ReadSignalBusAction> {
+	private readonly busses: BusByID
+	private readonly registers: RegisterByID
+
+	constructor(busses: BusByID, registers: RegisterByID) {
+		this.busses = busses
+		this.registers = registers
+	}
+
+	get actionType(): ActionType {
+		return ActionType.READ_SIGNAL
+	}
+
+	handle(action: ReadSignalBusAction): void {
+		const register = this.registers[action.into]
+		const bus = this.busses[action.fromBus]
+		assert(register.sizeBits <= bus.sizeBits)
+		assertSignal(bus.unsigned, action.fromBus)
+		register.unsigned = bus.unsigned
+	}
+}
