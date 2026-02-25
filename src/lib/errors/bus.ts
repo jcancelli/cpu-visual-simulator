548f04928fbdfdcba6406982a0d141e6540a2bd4
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-25 15:12:53 +0100

    Add bus signal conflict error

--- src/lib/errors/bus.ts
+++ src/lib/errors/bus.ts
@@ -1,11 +1,27 @@
+import { BusID } from "$lib/types/bus"
 import { CPUVSError } from "./cpuvs"
 
 /** Base class for errors regarding a {@link Bus} */
 export abstract class BusError extends CPUVSError {}
 
-/** Error regarding a read signal operation on a {@link Bus} that is not carrying a signal */
+/** Error regarding a send signal operation on a {@link Bus} that is already carrying a signal */
+export class SignalConflictError extends BusError {
+	/** The bus regarding the conflict of signals */
+	public readonly busID: BusID
+
+	constructor(busID: BusID) {
+		super(`Multiple signals being sent on bus "${BusID[busID]}"`)
+		this.busID = busID
+	}
+}
+
+/** Error regarding a read/end signal operation on a {@link Bus} that is not carrying a signal */
 export class NoSignalError extends BusError {
-	constructor() {
-		super("Trying to read from a bus with no signal")
+	/** The bus with no signal */
+	public readonly busID: BusID
+
+	constructor(busID: BusID) {
+		super(`Tring to end/read signal from bus "${BusID[busID]}" that has no signal`)
+		this.busID = busID
 	}
 }
