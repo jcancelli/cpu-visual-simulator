62ca74691d57fb12887ad551981167ec5a6c24d7
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-27 18:41:19 +0100

    Add incrementer component

---
+++ src/lib/components/cpu/Incrementer.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Base, u8ToPaddedString, type U8 } from "$lib/types/integer"
+
+	export interface IncrementeProps {
+		/** ID of this element */
+		id?: string
+		/** Base used for the string representation of the +2 */
+		base?: Base
+	}
+
+	let { id, base = Base.DECIMAL }: IncrementeProps = $props()
+</script>
+
+<div
+	{id}
+	autocapitalize="characters"
+	class="
+		relative
+		h-7.5
+		w-25
+		rounded-md
+		border
+		border-black
+		bg-gray-100
+		text-center
+		font-mono
+		text-base
+		leading-7.5
+	"
+>
+	+{u8ToPaddedString(2 as U8, base)}
+</div>
