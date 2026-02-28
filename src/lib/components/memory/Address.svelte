c0bdf5571ada637b9aaf2a3d804fca017d61bd7e
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-02-28 16:37:55 +0100

    Implement memory address component

---
+++ src/lib/components/memory/Address.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Base, u8ToPaddedString, type U8 } from "$lib/types/integer"
+
+	export interface AddressProps {
+		address: U8
+		base?: Base
+		id?: string
+		class?: string
+	}
+
+	let { address, base, id, ...props }: AddressProps = $props()
+</script>
+
+<div
+	{id}
+	class="
+		memory-address
+		register
+		w-(--memory-address-width)
+		bg-memory-address
+		text-center
+		text-address-text
+		{props.class ?? ''}
+	"
+>
+	{u8ToPaddedString(address, base)}
+</div>
