763652716c28deaa189d2b609286e75c5273dd34
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-03-01 11:09:10 +0100

    Move tailwind stylesheet to src

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import "./layout.css"
+	import "../style.css"
 	import favicon from "$lib/assets/favicon.png"
 	import { page } from "$app/state"
 	import { locales, localizeHref } from "$lib/paraglide/runtime"
