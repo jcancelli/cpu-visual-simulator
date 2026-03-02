abc90c420af2f9d4ca6b2281391acd2e1dc2f8d0
Jonathan Cancelli <honathan.cancelli.46@gmail.com>
2026-03-02 16:44:36 +0100

    Move Flashable related stuff into single animation module

--- src/lib/flash/animation.ts
+++ src/lib/flash/animation.ts
@@ -1,5 +1,17 @@
 import { EMPTY_FUNC, unreachable } from "$lib/util/development"
-import type { FlashableID } from "./flashable"
+
+/** An element that can perform a flash animation */
+export interface Flashable {
+	/** @returns The ID of this element */
+	getFlashableID(): FlashableID
+	/** @returns A new instance of a flash animation */
+	createFlashAnimation(): FlashAnimation
+	/** @returns All of the subelements associated with this element */
+	getFlashableSubelements(): Flashable[]
+}
+
+/** ID of a {@link Flashable} element */
+export type FlashableID = string
 
 /** Subset of {@link Animation} relevant for a flasah animation */
 export interface FlashAnimation {
